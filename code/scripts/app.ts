ee866f5318908fda9d0abd2920537d6feda8a613
Khushansh <khushanshbaliyan@gmail.com>
2025-06-20 16:23:00 +0530

    Enhance website layout and animations: Added navigation menu, hero section with video, and cursor animations. Updated styles for improved responsiveness and visual appeal. Integrated GSAP for animation effects.

--- code/scripts/app.ts
+++ code/scripts/app.ts
@@ -1,3 +1,100 @@
-import gsap from "gsap";
+// import gsap from "gsap";
 
-console.log('hello');
+
+
+
+// import gsap from "gsap";
+
+// import gsap from "gsap";
+
+// @ts-ignore
+
+const nav = document.querySelector('nav') as HTMLElement;
+// @ts-ignore
+function bignavbarAnimation() {
+const mainNav = document.querySelector('.main-nav') as HTMLElement;
+const menuOption = document.getElementById('menu-option') as HTMLButtonElement;
+
+console.log(menuOption.innerHTML)
+console.log(menuOption)
+
+
+
+
+const timeline = gsap.timeline({ paused: true, reversed: false });
+    timeline.addLabel('lol') 
+timeline.to(mainNav,{
+        y:'10%',
+        duration:0.3
+    },'lol')
+ 
+    timeline.to(nav,{
+        backgroundColor:'#000',
+        color:'#fff',
+        duration:0.3,
+        
+    },'lol')
+    timeline.to(menuOption,{
+        backgroundColor:'#000',
+        color:'#fff',
+        duration:0.1
+    },)
+menuOption.addEventListener('click',(e) =>{
+    console.log(e);
+    if(timeline.reversed()){
+        timeline.play();
+        console.log(menuOption.innerHTML);
+        menuOption.innerHTML ='<i class="fa-solid fa-xmark"></i>'
+    }
+    else{
+    timeline.reverse();
+    menuOption.innerHTML = '<i class="fa-solid fa-bars"></i>'
+    }
+})
+}
+function navbarAnimation(){
+    gsap.from(nav,{
+        y:-100,
+        opacity:0,
+        duration:0.77
+    })
+}
+function heroSectionAnimation(){
+    gsap.from("#hero h1", {
+        y: 30,
+        opacity: 0,
+        stagger: .77,
+        duration: .77
+    });
+}
+function cursorAnimation() {
+    const video = document.querySelector('#hero video') as HTMLVideoElement;
+    const cursor = document.querySelector('.cursor')
+    video.addEventListener('mouseenter',(e) => {
+        console.log(e)
+        gsap.to(cursor,{
+           opacity:1,
+           scale:1
+        })
+    })
+    video.addEventListener('mouseleave',(e) =>{
+        console.log(e)
+         gsap.to(cursor,{
+           opacity:0,
+           scale:0
+        })
+    })
+    video.addEventListener('mousemove',(e) =>{
+        gsap.to(cursor,{
+            // x:e.x - 250,
+            // y:e.y + 250,
+            top:e.clientY ,
+            left:e.clientX 
+        })
+    })
+}
+cursorAnimation()
+heroSectionAnimation()
+navbarAnimation()
+bignavbarAnimation()
+export{}
